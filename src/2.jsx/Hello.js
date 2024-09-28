fd865b8c3efba42a4cc40f0588898733cd6fd374
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-09-28 14:43:33 +0530

    Added jsx

---
+++ src/2.jsx/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hello = () =>{
+    // return (
+    //     <div id="divId" className="divHeader">
+    //         <h1>This is using JSX</h1>
+    //     </div>
+    // )
+
+    return React.createElement('div', {id: 'divId', className:'divHeader'}, 
+           React.createElement('h1', null, 'Without using JSX')
+    )
+    
+
+}
+
+export default Hello
